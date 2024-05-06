423804733cd8d56ea8ae20614e263c5ad6b3376e
simodevjs <simodevjs@gmail.com>
2024-05-06 05:01:55 +0100

    jjjjj

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,32 @@
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def test_id_assignment(self):
+        """Test that each BaseModel has a unique id"""
+        model1 = BaseModel()
+        model2 = BaseModel()
+        self.assertNotEqual(model1.id, model2.id)
+
+    def test_to_dict_contains_correct_keys(self):
+        """Test that the dictionary representation includes correct keys"""
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIn("id", model_dict)
+        self.assertIn("__class__", model_dict)
+        self.assertIn("created_at", model_dict)
+        self.assertIn("updated_at", model_dict)
+"""
+    def test_to_dict_contains_correct_types(self):
+        //Test that the dictionary representation contains correct types for datetime fields//
+        model = BaseModel()
+        model_dict = model.to_dict()
+        for key in ["created_at", "updated_at"]:
+            self.assertIsInstance(datetime.fromisoformat(model_dict[key]), datetime)
+"""
+
+if __name__ == '__main__':
+    unittest.main()
