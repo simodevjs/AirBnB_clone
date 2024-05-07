6b530faebc38954e011fe0e25712c04a141fc53f
simodevjs <simodevjs@gmail.com>
2024-05-07 02:44:11 +0100

    szszszs

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,13 @@
 import uuid
 import datetime
+import models
 class BaseModel:
     """A base class for all hbnb models."""
-
     def __init__(self, *args, **kwargs):
         """Initialize the BaseModel with id, created_at, and updated_at attributes."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def __init__(self, *args, **kwargs):
-        """Initialize the BaseModel with id, created_at, and updated_at attributes."""
+        self.updated_at = self.created_at
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -20,10 +15,7 @@ class BaseModel:
                         # Convert string datetime to datetime object
                         value = datetime.datetime.fromisoformat(value)
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = self.created_at
+        models.storage.new(self)       
 
     def __str__(self):
         """Return a string representation of the BaseModel instance."""
@@ -44,10 +36,10 @@ class BaseModel:
     def save(self):
         """Update the updated_at attribute to the current datetime."""
         self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Return a dictionary of the instance attributes for serialization"""
-        
         result = {k: v for k, v in reversed(self.__dict__.items())
                   if k not in ['id', 'created_at', 'updated_at']}
         result['__class__'] = self.__class__.__name__ 
---
+++ models/engine/file_storage.py
@@ -0,0 +1,37 @@
+import json
+from os import path
+from models.base_model import BaseModel
+
+class FileStorage:
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file"""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(serialized_objects, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        if path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                try:
+                    loaded_objects = json.load(f)
+                    for key, obj_dict in loaded_objects.items():
+                        class_name, obj_id = key.split('.')
+                        obj_class = globals()[class_name]
+                        self.__objects[key] = obj_class(**obj_dict)
+                except json.JSONDecodeError:
+                    pass 
