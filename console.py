37e907cff802655962f0677c9e92f56fe0d12164
simodevjs <simodevjs@gmail.com>
2024-05-07 04:04:17 +0100

    dedede

--- console.py
+++ console.py
@@ -2,18 +2,22 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """Simple command processor for HBNB clone."""
+    """Simple command processor for the HBNB clone."""
 
-    prompt = "(hbnb) "  # Custom command prompt
+    prompt = "(hbnb) "  
+
     def do_quit(self, arg):
         """Quit command to exit the program."""
-        return True  # Returning True exits the cmdloop
+        return True  
+
     def do_EOF(self, arg):
         """Exit the program when receiving EOF (End of File) signal."""
-        print()  # Print a newline for clean exit
-        return True  # Returning True exits the cmdloop
+        print()  
+        return True  
+
     def emptyline(self):
         """Do nothing upon receiving an empty line."""
         pass
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
